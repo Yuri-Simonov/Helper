bc9c1847b2bde494388e06499ecaaf77ef220e51
Uriy Simonov <yura15-96@mail.ru>
2023-04-03 18:06:36 +0300

    основа под цветовые решения оформления примеров кода

---
+++ example.js
@@ -0,0 +1,14 @@
+Object.prototype.sayHello = function(){...}
+
+function name1(a,b) {
+	console.log(ddasdasd);
+}
+
+const a = a - b; 
+let v1 = 1 - 10; // fg
+
+let b = {
+	a: 'fgdfgdfg'
+}
+
+name1(1,2)
