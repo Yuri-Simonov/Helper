335051086f0f30bc907755ca840266799acfc232
Yuri Simonov <yura15-96@mail.ru>
2023-05-10 18:16:54 +0300

    доделан вопрос по кастомному контролу (#71)

--- src/app/angular/questions/form/custom_control/custom_control.component.ts
+++ src/app/angular/questions/form/custom_control/custom_control.component.ts
@@ -1,23 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
     selector: 'app-custom-control',
     templateUrl: './custom_control.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CustomControlComponent implements ControlValueAccessor {
-    writeValue(obj: any): void {
-        throw new Error('Method not implemented.');
-    }
-    registerOnChange(fn: any): void {
-        throw new Error('Method not implemented.');
-    }
-    registerOnTouched(fn: any): void {
-        throw new Error('Method not implemented.');
-    }
-    setDisabledState?(isDisabled: boolean): void {
-        throw new Error('Method not implemented.');
-    }
+export class CustomControlComponent {
     panelOpenState: boolean = false;
 }
