dc25660e9467d3b2f7addfa2e1658490a10f3a1c
Yuri Simonov <yura15-96@mail.ru>
2023-07-01 15:21:45 +0300

    добавлена инфа по cherry-pick (#103)

--- src/app/git/questions/all.ts
+++ src/app/git/questions/all.ts
@@ -341,4 +341,45 @@ git commit -m "<span class="string">название_коммита</span>" <spa
 	</p>`,
         selected: false,
     },
+    {
+        title: 'Как перенести коммиты в другую ветку?',
+        body: `<p>
+		Для переноса (копирования) коммита (в котором присутствуют
+		нужные вам наработки) из одной ветки в другую, в
+		<code>git</code> существует следующая команда:
+	</p>
+	<pre><code>git cherry-pick <span class="string">хэш_коммита</span>
+git cherry-pick <span class="string">99fb0814f8cd10eaeda0c5f89373ee2b42d7fde9</span> <span class="comment">// пример</span></code></pre>
+	<p>
+		Важное уточнение:
+		<span class="attention"
+			>команда должна выполняться в той ветке, в которую вы хотите
+			добавить коммит</span
+		>.
+	</p>
+	<p>
+		Если при переносе коммита вы хотите дать ему новое название, то
+		для этого нужно добавить флаг <code>-edit</code>:
+	</p>
+	<pre><code>git cherry-pick <span class="string">хэш_коммита</span> -edit</code></pre>
+	<p>
+		Если при переносе коммита вы не хотите сразу создавать новый
+		коммит в текущей ветке, а хотите его как-то подредактировать, то
+		для этого тоже есть специальный флаг <code>--no-commit</code>:
+	</p>
+	<pre><code>git cherry-pick <span class="string">хэш_коммита</span> --no-commit</code></pre>
+	<p>
+		При данной команде все появившиеся изменения после переноса
+		нужно будет самим потом добавить в коммит с помощью
+		соответствующих команд.
+	</p>
+	<p>
+		Используйте данную команду с умом, т.к. она
+		<span class="attention"
+			>не является заменой <code>merge</code> или
+			<code>rebase</code></span
+		>.
+	</p>`,
+        selected: false,
+    },
 ];
