5e68a15e61bc73c0b05d124f2900604f4c66fb33
Yuri Simonov <yura15-96@mail.ru>
2023-07-01 20:20:08 +0300

    добавлены анимации при переходах между страницами (#104)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { opacityAnimation } from 'src/app/shared/animations/opacity.animation';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    animations: [opacityAnimation],
 })
 export class AppComponent implements OnInit {
     ngOnInit(): void {}
+
+    getRouterOutletState(outlet: RouterOutlet) {
+        return outlet.isActivated ? outlet.activatedRoute : '';
+    }
 }
---
+++ src/app/shared/animations/opacity.animation.ts
@@ -0,0 +1,47 @@
+import {
+    trigger,
+    animate,
+    transition,
+    style,
+    query,
+} from '@angular/animations';
+
+export const opacityAnimation = trigger('opacityAnimation', [
+    transition('* => *', [
+        query(
+            ':enter',
+            [style({ opacity: 0, position: 'absolute', width: '100%' })],
+            {
+                optional: true,
+            }
+        ),
+
+        query(
+            ':leave',
+            [
+                style({ opacity: 1 }),
+                animate(
+                    '0.1s',
+                    style({
+                        opacity: 0,
+                        position: 'absolute',
+                        width: '100%',
+                    })
+                ),
+            ],
+            { optional: true }
+        ),
+
+        query(
+            ':enter',
+            [
+                style({ opacity: 0 }),
+                animate(
+                    '0.2s',
+                    style({ opacity: 1, position: 'relative', width: '100%' })
+                ),
+            ],
+            { optional: true }
+        ),
+    ]),
+]);
