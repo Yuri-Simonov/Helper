14116df877c7300ee8eb489f2d4f0f8b51e81738
Yuri Simonov <yura15-96@mail.ru>
2023-11-12 20:13:08 +0300

    добавлена информация по атр.директиве ngClass (#141)

--- src/app/angular/questions/directive.ts
+++ src/app/angular/questions/directive.ts
@@ -476,6 +476,63 @@ arr: <span class="type">number[]</span> <span class="operator">=</span> <span cl
         selected: false,
         lastUpdate: '15.10.2023',
     },
+    {
+        title: 'Атрибутивная директива <span class="variable">ngClass</span>',
+        body: `<p>
+                <span class="attention"
+                    >Атрибутивная директива <code>ngClass</code> позволяет как
+                    статически, так и динамически изменять css-классы на
+                    элементе DOM-дерева в зависимости от каких-либо
+                    условий</span
+                >.
+            </p>
+            <p>
+                В качестве значений <code>ngClass</code> может принимать строки,
+                массивы и объекты.
+            </p>
+            <i class="subtitle">Примеры со строками</i>
+            <pre><code><span class="tag">&lt;some-element</span> <span class="keyword">ngClass</span><span class="operator">=</span><span class="string">"first second"</span><span class="tag">></span>...<span class="tag">&lt;some-element></span> <span class="comment">// статический вариант записи</span>
+<span class="tag">&lt;some-element</span> <span class="keyword">[ngClass]</span><span class="operator">=</span><span class="string">"'first second'"</span><span class="tag">></span>...<span class="tag">&lt;some-element></span> <span class="comment">// динамический вариант записи</span></code></pre>
+            <p>
+                Как видно из примера выше, просто перечисляем css-классы внутри
+                кавычек, которые нужно добавить на элемент DOM-дерева.
+            </p>
+            <i class="subtitle">Пример с массивом</i>
+            <pre><code><span class="tag">&lt;some-element</span> <span class="keyword">[ngClass]</span><span class="operator">=</span><span class="string">"['first', 'second']"</span><span class="tag">></span>...<span class="tag">&lt;some-element></span></code></pre>
+            <p>
+                Здесь уже нужные css-классы перечисляются как элементы массива.
+            </p>
+            <i class="subtitle">Примеры с объектами</i>
+            <p>
+                Варианты записи, когда в качестве значений для
+                <code>ngClass</code> указываются строки или массивы, практически
+                не встречаются, т.к. не позволяют использовать условия, в
+                зависимости от которых будет добавляться или убираться тот или
+                иной css-класс. Объекты же позволяют это делать, поэтому в
+                основном они и используются.
+            </p>
+            <pre><code><span class="tag">&lt;some-element</span> <span class="keyword">[ngClass]</span><span class="operator">=</span><span class="string">"{'first': true, 'second': false}"</span><span class="tag">></span>...<span class="tag">&lt;some-element></span></code></pre>
+            <p>
+                В примере выше для каждого css-класса задается условие его
+                отображения. Если оно правдиво (<code>true</code>), то css-класс
+                добавляется на элемент DOM-дерева. Если условие ложно, тогда
+                css-класс не добавляется.
+            </p>
+            <pre><code><span class="tag">&lt;some-element</span> <span class="keyword">[ngClass]</span><span class="operator">=</span><span class="string">"{'class1 class2' : true}"</span><span class="tag">></span>...<span class="tag">&lt;some-element></span></code></pre>
+            <p>
+                В примере выше показано, что <code>ngClass</code> также
+                позволяет отображать или скрывать сразу несколько css-классов в
+                зависимости от одного и того же условия.
+            </p>`,
+        selected: false,
+        lastUpdate: '12.11.2023',
+    },
+    {
+        title: 'Атрибутивная директива <span class="variable">ngStyle</span>',
+        body: ``,
+        selected: false,
+        lastUpdate: '12.11.2023',
+    },
     // {
     //     title: '',
     //     body: ``,
