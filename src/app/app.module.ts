b9a38e0df5a54a39d35248d26a61689fe0e57960
Yuri Simonov <yura15-96@mail.ru>
2024-01-14 20:59:49 +0300

    прокинуты модули

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,8 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderModule } from 'src/app/shared/modules/header/header.module';
 import { FooterModule } from 'src/app/shared/modules/footer/footer.module';
-import { JavascriptModule } from 'src/app/theory/javascript/javascript.module';
-import { AngularModule } from 'src/app/theory/angular/angular.module';
-import { GitModule } from 'src/app/theory/git/git.module';
 import { HomeModule } from 'src/app/home/home.module';
+import { TheoryModule } from './theory/theory.module';
 import { MaterialModule, SidenavModule } from '@modules';
 
 @NgModule({
@@ -21,12 +19,10 @@ import { MaterialModule, SidenavModule } from '@modules';
         BrowserAnimationsModule,
         HeaderModule,
         FooterModule,
-        JavascriptModule,
-        AngularModule,
-        GitModule,
         MaterialModule,
         HomeModule,
         SidenavModule,
+        TheoryModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
