ca5435fbc27bc160af0484016d3409ac0578174f
Yuri Simonov <yura15-96@mail.ru>
2024-04-16 21:26:13 +0300

    перенесена и обновлена инфа по оператору zip

--- src/app/for-examples/for-examples/for-examples.component.ts
+++ src/app/for-examples/for-examples/for-examples.component.ts
@@ -7,6 +7,7 @@ import {
     delay,
     exhaustMap,
     flatMap,
+    from,
     interval,
     map,
     merge,
@@ -14,6 +15,7 @@ import {
     of,
     switchMap,
     tap,
+    zip,
 } from 'rxjs';
 
 @Component({
@@ -24,30 +26,41 @@ import {
 })
 export class ForExamplesComponent {
     ngOnInit() {
-        of('A', 'B', 'C').pipe(
-            exhaustMap((value) => {
-                return of(value).pipe(
-                    delay(1000),
-                    map((currentValue) => 'Текущее значение потока: ' + currentValue),
-                );
-            }),
-        );
-        // .subscribe(console.log);
+        //     of('A', 'B', 'C').pipe(
+        //         exhaustMap((value) => {
+        //             return of(value).pipe(
+        //                 delay(1000),
+        //                 map((currentValue) => 'Текущее значение потока: ' + currentValue),
+        //             );
+        //         }),
+        //     );
+        //     .subscribe(console.log);
+
+        from([1, 2, 3]) // Создаем Observable из массива чисел
+            .pipe(
+                map((number) => number * 2), // Умножаем каждое число на 2
+                // zip(from(['a', 'b', 'c'])), // Комбинируем текущий поток с другим Observable
+            )
+            .subscribe(console.log); // Выводим результат
     }
 
-    first = new Observable((subscriber) => {
-        setTimeout(() => {
-            subscriber.next('Событие из первого потока');
-            subscriber.complete();
-        }, 500);
-    });
+    // first = new Observable((subscriber) => {
+    //     setTimeout(() => {
+    //         subscriber.next('Событие из первого потока');
+    //         subscriber.complete();
+    //     }, 500);
+    // });
 
-    second = new Observable((subscriber) => {
-        setTimeout(() => {
-            subscriber.next('Событие из второго потока');
-            subscriber.complete();
-        }, 200);
-    });
+    // second = new Observable((subscriber) => {
+    //     setTimeout(() => {
+    //         subscriber.next('Событие из второго потока');
+    //         subscriber.complete();
+    //     }, 200);
+    // });
 
-    result = merge(this.first, this.second).subscribe(console.log);
+    // first = of('A', 'B', 'C');
+    // second = of('1', '2', '3', 4);
+
+    // result = zip(this.first, this.second).subscribe(console.log);
+    // result2 = zip(this.third, this.forth).subscribe(console.log);
 }
--- src/app/theory/questions/angular/rxjs-operators.ts
+++ src/app/theory/questions/angular/rxjs-operators.ts
@@ -316,6 +316,52 @@ export const rxjsOperatorsQuestions: IQuestion[] = [
         selected: false,
         lastUpdate: '16.04.2024',
     },
+    {
+        title: 'zip',
+        body: `<p>
+                Оператор <code>zip</code>
+                <span class="attention"
+                    >объединяет 2 или более потоков, если в каждом из переданных потоков есть доступные события для
+                    создания пары</span
+                >.
+            </p>
+            <img src="assets/img/angular/rxjs/zip.png" alt="оператор zip" />
+            <p>
+                На картинке выше есть 2 потока. В первом потоке происходит событие "1", но оно не попадет в объединенный
+                поток, пока в другом потоке не произойдет событие "А", то есть, пока не создастся пара.
+            </p>
+            <p>Пример:</p>
+            <pre><code class="language-typescript">export class SomeComponent {
+    first = of('A', 'B', 'C');
+    second = of(1, 2, 3, 4);
+
+    result = zip(this.first, this.second).subscribe(console.log);
+}</code></pre>
+            <p>Результат выполнения кода в консоли:</p>
+            <pre><code class="language-typescript">['A', 1]
+['B', 2]
+['C', 3]</code></pre>
+            <p>
+                В примере выше во втором потоке для элемента "<code>4</code>" не нашлось в итоге пары из первого потока,
+                поэтому его значение и не вывелось в консоли.
+            </p>`,
+        selected: false,
+        lastUpdate: '16.04.2024',
+    },
+    {
+        title: 'combineLatest',
+        body: ``,
+        selected: false,
+        lastUpdate: '16.04.2024',
+        disabled: true,
+    },
+    {
+        title: 'forkJoin',
+        body: ``,
+        selected: false,
+        lastUpdate: '16.04.2024',
+        disabled: true,
+    },
     // {
     //     title: '',
     //     body: ``,
