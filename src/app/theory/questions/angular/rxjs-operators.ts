e5f46b9908d87dd50977fdea1bf8a5de8002104c
Yuri Simonov <yura15-96@mail.ru>
2024-04-30 16:02:25 +0300

    добавлена инфа по оператору fromEvent

--- src/app/theory/questions/angular/rxjs-operators.ts
+++ src/app/theory/questions/angular/rxjs-operators.ts
@@ -703,10 +703,60 @@ export const rxjsOperatorsQuestions: IQuestion[] = [
     },
     {
         title: '<span class="variable">fromEvent</span>',
-        body: `1`,
+        body: `<p>
+                <span class="attention"
+                    >Оператор <code>fromEvent</code> используется для создания <code>Observable</code> из событий,
+                    происходящих в DOM-дереве</span
+                >, таких как клик мыши, нажатие клавиш и т.д.
+            </p>
+            <img src="assets/img/angular/rxjs/fromEvent.png" alt="оператор fromEvent" />
+            <p>
+                У данного оператора есть 2 обязательных параметра (также есть еще 2 опциональных параметра, но они
+                используются крайне редко):
+            </p>
+            <ul>
+                <li>
+                    <span class="attention">target</span> - задает элемент DOM-дерева или
+                    глобальный объект (например, <code>document</code>), у которого необходимо отслеживать происходящие
+                    в нем события;
+                </li>
+                <li>
+                    <span class="attention">eventName</span> - задает конкретный тип
+                    прослушиваемого события. Строковый тип данных.
+                </li>
+            </ul>
+            <p>Пример с кликом по глобальному объекту <code>document</code>:</p>
+            <pre><code class='language-typescript'>export class ForExamplesComponent {
+    ngOnInit() {
+        const clicks = fromEvent(document, 'click');
+        clicks.subscribe(console.log);
+    }
+}
+</code></pre>
+            <p>В консоли мы увидим объект с типом <code>PointerEvent</code>:</p>
+            <pre><code class='language-typescript'>PointerEvent {...}</code></pre>
+            <p>
+                В примере выше, внутри "<code>...</code>" будет содержаться вся информация о произошедшем на странице
+                событии клика.
+            </p>
+            <p>Пример с кликом по найденному элементу DOM-дерева с помощью метода <code>querySelector</code>:</p>
+            <pre><code class='language-typescript'>export class ForExamplesComponent {
+    ngOnInit() {
+        const body = document.querySelector('body');
+
+        if (body) {
+            const clicks = fromEvent(body, 'click');
+            clicks.subscribe(console.log);
+        }
+    }
+}</code></pre>
+            <p>
+                В консоли мы также увидим объект с типом <code>PointerEvent</code>, но данные в нем будут уже другие, т.к.
+                координаты клика, с высокой долей вероятности, будут отличаться:
+            </p>
+            <pre><code class='language-typescript'>PointerEvent {...}</code></pre>`,
         selected: false,
-        lastUpdate: '29.04.2024',
-        disabled: true,
+        lastUpdate: '30.04.2024',
         footerText: ['Дополнительный материал', 'Дополнительные материалы'],
         footerLinks: [
             {
