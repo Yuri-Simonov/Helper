540c18ea69f408eac8b487426fe73717eedee6a7
Yuri Simonov <yura15-96@mail.ru>
2024-05-01 18:14:40 +0300

    дополнение инфы по операторам

--- src/app/for-examples/for-examples/for-examples.component.ts
+++ src/app/for-examples/for-examples/for-examples.component.ts
@@ -29,33 +29,20 @@ import {
     styleUrl: './for-examples.component.scss',
 })
 export class ForExamplesComponent {
-    // ngOnInit() {
-    //     fromEvent(document, 'click')
-    //         .pipe(take(2))
-    //         .subscribe({
-    //             next: () => console.log('Сделан клик'),
-    //             error: () => console.log('Ошибка'),
-    //             complete: () => console.log('Поток завершен'),
-    //         });
-    // }
-
-    // ngOnInit() {
-    //     const numbers = [1, 2, 3];
-    //     const observableNumbers = from(numbers);
-    //     observableNumbers.subscribe({
-    //         next: (value) => console.log('Текущее значение: ', value),
-    //         error: (error) => console.log('Ошибка: ', error),
-    //         complete: () => console.log('Поток завершен'),
-    //     });
-    // }
-
-    someObservable: Observable<any> = of(1, 'текст', ['123', 0]);
-
     ngOnInit() {
-        this.someObservable.subscribe({
-            next: (value) => console.log('Текущее значение: ', value),
-            error: (error) => console.log('Ошибка: ', error),
-            complete: () => console.log('Поток завершен'),
-        });
+        of('A', 'B', 'C')
+            .pipe(
+                exhaustMap((value) => {
+                    return of(value).pipe(
+                        delay(1000), // создаем искусственную задержку в 1 секунду
+                        map((currentValue) => 'Текущее значение потока: ' + currentValue),
+                    );
+                }),
+            )
+            .subscribe({
+                next: (value) => console.log('Текущее значение: ', value),
+                error: (error) => console.log('Ошибка: ', error),
+                complete: () => console.log('Поток завершен'),
+            });
     }
 }
