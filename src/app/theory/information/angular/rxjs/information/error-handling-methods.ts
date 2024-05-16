8689f1973fb0dca3c24821a1a0c24e490d164729
Yuri Simonov <yura15-96@mail.ru>
2024-05-16 16:16:17 +0300

    обновлена инфа по способам обработки ошибок с помощью операторов rxjs

--- src/app/theory/information/angular/rxjs/information/error-handling-methods.ts
+++ src/app/theory/information/angular/rxjs/information/error-handling-methods.ts
@@ -3,38 +3,16 @@ import { IInfo } from '@types';
 export const ERROR_HANDLING_METHODS: IInfo = {
     title: 'Способы обработки ошибок в <span class="variable">RxJs</span>',
     body: `<p>
-                Чтобы ваше приложение "не падало" из-за возможной ошибки
-                (например, сервер не отвечает на ваш запрос), эти самые ошибки
-                можно и нужно обрабатывать.
+                Чтобы приложение "не падало" из-за возможной ошибки (например, сервер не отвечает на запрос), эти самые
+                ошибки можно и нужно обрабатывать, предусматривая всевозможные сценарии их возникновения.
             </p>
-            <p>В Angular для обработки ошибок существуют следующие операторы: <code class="dialog" dialog_angular_rxjs-operators_catch-error>catchError</code>, <code class="dialog" dialog_angular_rxjs-operators_retry>retry</code>, <code class="dialog" dialog_angular_rxjs-operators_retry-when>retryWhen</code> и <code class="dialog" dialog_angular_rxjs-operators_on-error-resume-next-with>onErrorResumeNextWith
-</code></p>
-            <i class="subtitle">Оператор onErrorResumeNext / onErrorResumeNextWith</i>
             <p>
-                Суть данного оператора заключается в том, что
-                <span class="attention"
-                    >когда происходит какая-то ошибка, он переключается на
-                    другой <code>Observable</code></span
-                >
-                (своего рода запасной/страхующий поток).
-            </p>
-            <pre><code class="language-typescript">source = new Observable(subscriber => {
-	console.log('попытка подключения');
-	subscriber.error('ошибка!');
-});
-
-planB = of('План "Б" на случай ошибки');
-
-subscription = source.pipe(onErrorResumeNext(planB))
-	.subscribe(
-		val => console.log('next:', val),
-		err => console.error('error:', err),
-		() => console.log('completed: поток завершен')
-	);</code></pre>
-            <p>Результат выполнения кода из примера выше:</p>
-            <pre><code class="language-typescript">// попытка подключения
-// ошибка!
-// completed: поток завершен</code></pre>`,
+                В библиотеке RxJS для обработки ошибок существуют отдельный тип операторов, в который входят такие
+                операторы, как:
+                <code class="dialog" dialog_angular_rxjs-operators_catch-error>catchError</code>,
+                <code class="dialog" dialog_angular_rxjs-operators_retry>retry</code> и
+                <code class="dialog" dialog_angular_rxjs-operators_retry-when>retryWhen</code>
+            </p>`,
     selected: false,
-    lastUpdate: '14.05.2024',
+    lastUpdate: '16.05.2024',
 };
