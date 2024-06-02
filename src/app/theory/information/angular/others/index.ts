08d0ff8a039edcf448dd1725aa2ec238ed30fd4c
Yuri Simonov <yura15-96@mail.ru>
2024-06-02 14:11:27 +0300

    добавление инфы про глобальные стили

--- src/app/theory/information/angular/others/index.ts
+++ src/app/theory/information/angular/others/index.ts
@@ -6,11 +6,14 @@ import { COMPILATION_MECHANISMS } from './information/compilation-mechanisms';
 import { DEPENDENCY_INJECTION } from './information/dependency-injection';
 import { CONNECTING_SCRIPTS_AND_STYLES } from './information/connecting-scripts-and-styles';
 import { RENDERER_2 } from './information/renderer-2';
+import { SOME_GLOBAL_STYLE_FILES } from './information/some-global-style-files';
 
 export const othersInfo: IInfo[] = [
     { chapter: 'Общие понятия' },
     ANGULAR_CLI,
     MODULES_IN_ANGULAR,
+    { chapter: 'Стили' },
+    SOME_GLOBAL_STYLE_FILES,
     { chapter: 'Прочее' },
     COMPILATION_MECHANISMS,
     DEPENDENCY_INJECTION,
