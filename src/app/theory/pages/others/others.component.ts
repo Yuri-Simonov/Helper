f3d7a74493de51b7cfab932fa73261a370e370a6
Yuri Simonov <yura15-96@mail.ru>
2024-07-20 16:54:20 +0300

    добавлен раздел  на главной + инфа по авторизации

--- src/app/theory/pages/others/others.component.ts
+++ src/app/theory/pages/others/others.component.ts
@@ -1,9 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { IList } from '../../../shared/types';
+
+import { allOthersInfo } from '../../information/others';
+
 @Component({
     selector: 'app-others',
     templateUrl: './others.component.html',
     styleUrls: ['./others.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OthersComponent {}
+export class OthersComponent {
+    list: IList[] = [{ name: 'Все', path: 'others', info: allOthersInfo }];
+}
