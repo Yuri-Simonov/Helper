50a23fbebb32af02602bbf28c499fd22b79bae3d
Yuri Simonov <80687042+Yuri-Simonov@users.noreply.github.com>
2024-07-21 14:01:31 +0300

    обновлена инфа по subject из rxjs + мелкие правки (#92)

--- src/app/theory/information/angular/rxjs/information/types-of-subject.ts
+++ src/app/theory/information/angular/rxjs/information/types-of-subject.ts
@@ -2,57 +2,63 @@ import { IInfo } from '@types';
 
 export const TYPES_OF_SUBJECT: IInfo = {
     title: 'Разновидности типа <span class="variable">Subject</span> и их различия',
-    body: `<p>В RxJS имеется несколько разновидностей <code>Subject</code>:</p>
+    body: `<p>В библиотеке RxJS имеется несколько разновидностей класса <code>Subject</code>:</p>
         <ul>
-            <li><span class="attention">BehaviorSubject</span></li>
-            <li><span class="attention">ReplaySubject</span></li>
-            <li><span class="attention">AsyncSubject</span></li>
+            <li><span class="attention">Subject</span> (наследуется от <code>Observable</code>);</li>
+            <li><span class="attention">BehaviorSubject</span> (наследуется от <code>Subject</code>);</li>
+            <li><span class="attention">ReplaySubject</span> (наследуется от <code>Subject</code>);</li>
+            <li><span class="attention">AsyncSubject</span> (наследуется от <code>Subject</code>).</li>
         </ul>
         <i class="subtitle">Subject</i>
         <p>
-            RxJS <code>Subject</code> является разновидностью объектов
-            <code>Observable</code>. Особенность <code>Subject</code> в том, что
-            он может отправлять данные одновременно множеству "потребителей",
-            которые могут регистрироваться уже в процессе исполнения
-            <code>Subject</code>, в то время как исполнение стандартного
+            Особенностью класса <code>Subject</code> (и его разновидностей из списка выше) в том, что
+            <span class="attention">
+				он может отправлять данные одновременно нескольким подписчикам (наблюдателям),
+				которые могут регистрироваться уже в процессе работы
+			</span>
+            <code>Subject</code>, в то время как работа стандартного
             <code>Observable</code> осуществляется уникально для каждого его
             вызова.
         </p>
         <pre><code class="language-typescript">const subject$ = new Subject();
 
 subject$.subscribe((value) => console.log('Первая подписка:', value));
-subject$.next(3);
+subject$.next(1);
 subject$.subscribe((value) => console.log('Вторая подписка:', value));
-subject$.next(9);</code></pre>
+subject$.next(2);</code></pre>
 <p>В консоли мы увидим следующее:</p>
-<pre><code class="language-typescript">Первая подписка: 3
-Первая подписка: 9
-Вторая подписка: 9</code></pre>
+<pre><code class="language-typescript">Первая подписка: 1
+Первая подписка: 2
+Вторая подписка: 2</code></pre>
         <i class="subtitle">BehaviorSubject</i>
         <p>
             <code>BehaviorSubject</code> —
             <span class="attention">
-                передает новому подписчику последнее значение и в качестве
-                аргумента принимает начальное значение </span
+				создается с начальным значением, которое будет сразу доступно подписчику (наблюдателю) при подписке. Если подписка какого-либо подписчика на <code>BehaviorSubject</code> происходит после вызова метода <code>next</code>, то такой подписчик получает последнее значение, которое было отправлено в поток.</span
             >.
         </p>
-        <pre><code class="language-typescript">const behaviorSubject$ = new BehaviorSubject(5);
+        <pre><code class="language-typescript">const behaviorSubject$ = new BehaviorSubject(1);
 
-behaviorSubject$.subscribe((value) => console.log('Первая подписка:', value));
-behaviorSubject$.subscribe((value) => console.log('Вторая подписка:', value));
-behaviorSubject$.next(9);</code></pre>
+        behaviorSubject$.subscribe((value) => console.log('Первая подписка:', value));
+        behaviorSubject$.subscribe((value) => console.log('Вторая подписка:', value));
+        behaviorSubject$.next(2);
+        behaviorSubject$.subscribe((value) => console.log('Третья подписка:', value));
+        behaviorSubject$.next(3);</code></pre>
 <p>В консоли мы увидим следующее:</p>
-<pre><code class="language-typescript">Первая подписка: 5
-Вторая подписка: 5
-Первая подписка: 9
-Вторая подписка: 9</code></pre>
+<pre><code class="language-typescript">Первая подписка: 1
+Вторая подписка: 1
+Первая подписка: 2
+Вторая подписка: 2
+Третья подписка: 2
+Первая подписка: 3
+Вторая подписка: 3
+Третья подписка: 3</code></pre>
         <i class="subtitle">ReplaySubject</i>
         <p>
             <code>ReplaySubject</code> —
             <span class="attention">
-                передает новому подписчику все предыдущие значения, принимаемый
-                параметр — количество предыдущих значений </span
-            >.
+                сохраняет в буфер определенное количество последних отправленных в поток значений, которое задается первым параметром при создании <code>ReplaySubject</code>, и передает их новым подписчикам</span
+            >. Вторым параметром можно задать время, спустя которое запоминаемые значения будут удаляться из буфера.
         </p>
         <pre><code class="language-typescript">const replaySubject$ = new ReplaySubject(2);
 
@@ -71,7 +77,7 @@ replaySubject$.subscribe((value) => console.log('Результат: ', value));
                 передает новому подписчику последнее значение, но только после
                 того, как будет вызван метод
             </span>
-            <code>complete</code>.
+            <code>complete</code>. Он полезен в ситуациях, когда нужно дождаться завершения какого-то процесса и получить его результат.
         </p>
        <pre><code class="language-typescript">const asyncSubject$ = new AsyncSubject();
 
@@ -85,5 +91,5 @@ asyncSubject$.complete();</code></pre>
 <p>В консоли мы увидим следующее:</p>
 <pre><code class="language-typescript">Результат: 9</code></pre>`,
     selected: false,
-    lastUpdate: '09.02.2024',
+    lastUpdate: '21.07.2024',
 };
