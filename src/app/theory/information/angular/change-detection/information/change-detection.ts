acd00de53571857a6e78e81e9423215e6193d04b
Yuri Simonov <yura15-96@mail.ru>
2024-08-03 14:09:17 +0300

    добавлены примеры для стратегии OnPush

--- src/app/theory/information/angular/change-detection/information/change-detection.ts
+++ src/app/theory/information/angular/change-detection/information/change-detection.ts
@@ -85,7 +85,105 @@ export const CHANGE_DETECTION: IInfo = {
                     асинхронных данных, они будут обновляться каждый раз при появлении новых значений;
                 </li>
                 <li><span class="attention">Использование сигналов</span>.</li>
-            </ul>`,
+            </ul>
+            <i class="subtitle">Пример для декоратора @Input()</i>
+            <p>В родительском компоненте создадим поток с помощью оператора <code>interval</code>:</p>
+            <pre><code class="language-javascript">// app.component.ts
+
+export class AppComponent {
+    interval$ = interval(1000);
+}</code></pre>
+            <p>И с помощью асинхронной пайпы будем передавать значения потока в дочерний компонент:</p>
+            <pre><code class="language-html">&lt;!-- app.component.html --&gt;
+
+@if (interval$ | async; as number) {
+	&lt;app-child [number]="number">&lt;/app-child>
+}</code></pre>
+            <p>
+                В дочернем компоненте, как обычно, обрабатываем входящие данные с помощью декоратора
+                <code>@Input()</code>:
+            </p>
+            <pre><code class="language-javascript">// child.component.ts
+
+export class ChildComponent {
+    @Input() number: number;
+
+	ngOnChanges() {
+		console.log(this.number);
+	}
+
+	ngDoCheck() {
+		console.log('ngDoCheck app-child');
+	}
+}</code></pre>
+            <p>
+                И в консоли каждую секунду будут появляться значение интервала и сообщение из метода
+                <code>ngDoCheck</code>. А если мы попадаем в метод <code>ngDoCheck</code>, значит и механизм
+                <code>Change Detection</code> проверил компонент на изменения.
+            </p>
+            <i class="subtitle">Пример для событий DOM-дерева</i>
+            <p>Добавим в шаблон корневого компонента кнопку и повесим на нее простой обработчик клика:</p>
+            <pre><code class="language-html">&lt;!-- app.component.html --&gt;
+
+&lt;button (click)="handleClick()">Click&lt;/button></code></pre>
+            <p>В классе родительского компонента реализуем метод для обработки события клика:</p>
+            <pre><code class="language-javascript">// child.component.ts
+
+export class AppComponent {
+	ngDoCheck() {
+		console.log('ngDoCheck app-root');
+	}
+
+	handleClick() {
+		console.log('Кнопка нажата!');
+	}
+}</code></pre>
+            <p>
+                После каждого нажатия на кнопку в консоли будут появляться сообщения из методов <code>ngDoCheck</code> и
+                <code>handleClick</code>.
+            </p>
+            <i class="subtitle">Пример для асинхронной пайпы</i>
+            <p>Вновь воспользуемся интервалом:</p>
+            <pre><code class="language-javascript">// app.component.ts
+
+export class AppComponent {
+    interval$ = interval(1000);
+}</code></pre>
+            <p>И выведем в шаблоне данное свойство, добавив к нему асинхронную пайпу:</p>
+            <pre><code class="language-html">&lt;!-- app.component.html --&gt;
+
+&lt;h1>{{ interval$ | async }}&lt;/h1></code></pre>
+            <p>И на странице в браузере каждую секунду будут отображаться новые значения из потока.</p>
+            <i class="subtitle">Пример для сигналов</i>
+            <p>
+                Создадим свойство и присвоим ему сигнал, значение которого можно изменять напрямую. Зададим ему значение
+                <code>0</code>:
+            </p>
+            <pre><code class="language-javascript">// app.component.ts
+
+export class AppComponent {
+    signal = signal(0);
+}</code></pre>
+            <p>
+                Далее в методе жизненного цикла <code>ngAfterViewInit</code> с помощью функции
+                <code>setTimeout</code> спустя 3 секунды установим новое значение для сигнала равное <code>1</code>:
+            </p>
+            <pre><code class="language-javascript">// app.component.ts
+
+export class AppComponent {
+    signal = signal(0);
+
+	ngAfterViewInit() {
+        setTimeout(() => {
+            this.sig.set(1);
+        }, 3000);
+    }
+}</code></pre>
+            <p>И т.к. наш сигнал - это обычная функция, следовательно, в шаблоне ее нужно просто вызвать:</p>
+            <pre><code class="language-html">&lt;!-- app.component.html --&gt;
+
+&lt;h1>{{ signal() }}&lt;/h1></code></pre>
+            <p>И спустя 3 секунды значение на странице браузера изменится с нуля на единицу.</p>`,
     selected: false,
     lastUpdate: '03.08.2024',
 };
