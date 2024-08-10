2e6134848663ddf48fa25779b15425ec2392a52c
Yuri Simonov <yura15-96@mail.ru>
2024-08-10 11:57:02 +0300

    добавлена инфа про свзять методов detach и detectChanges

--- src/app/theory/information/angular/change-detection/index.ts
+++ src/app/theory/information/angular/change-detection/index.ts
@@ -2,5 +2,12 @@ import { IInfo } from '@types';
 
 import { CHANGE_DETECTOR_REF } from './information/change-detector-ref';
 import { CHANGE_DETECTION } from './information/change-detection';
+import { DETACH_WITH_DETECT_CHANGES } from './information/detach-with-detect-changes';
 
-export const changeDetectionInfo: IInfo[] = [{ chapter: 'Прочее' }, CHANGE_DETECTION, CHANGE_DETECTOR_REF];
+export const changeDetectionInfo: IInfo[] = [
+    { chapter: 'Механизм <span class="variable">Change Detection</span>' },
+    CHANGE_DETECTION,
+    { chapter: 'Ручное управление механизмом <span class="variable">Change Detection</span>' },
+    CHANGE_DETECTOR_REF,
+    DETACH_WITH_DETECT_CHANGES,
+];
---
+++ src/app/theory/information/angular/change-detection/information/detach-with-detect-changes.ts
@@ -0,0 +1,55 @@
+import { IInfo } from '@types';
+
+export const DETACH_WITH_DETECT_CHANGES: IInfo = {
+    title: 'Вызов метода <span class="variable">detectChanges</span> после вызова метода <span class="variable">detach</span>',
+    body: `<p>
+                Вызов метода <code>detach</code> отключает отслеживание изменений в текущем компоненте и во всех
+                дочерних, даже если данные изменятся. Однако тут есть одно "но".
+                <span class="attention">
+                    Если вызвать метод
+                    <code>detectChanges</code> в компоненте, где ранее был вызван метод <code>detach</code> и после
+                    этого произошли какие-то изменения, то они отобразятся на странице, т.е., механизм
+                    <code>Change Detection</code> их отследит</span
+                >.
+            </p>
+            <pre><code class="language-typescript">// app.component.ts
+
+export class AppComponent {
+    title: string = 'Hello, world!';
+
+    constructor(private cdr: ChangeDetectorRef) {
+		this.cdr.detach();
+	}
+
+    ngAfterViewInit() {
+        this.title = 'Привет, мир!';
+    }
+
+	handleClick() { 
+		this.cdr.detectChanges();
+	}
+}</code></pre>
+            <pre><code class="language-html">&lt;!-- app.component.html -->
+
+&lt;h1>Заголовок: {{ title }}&lt;/h1>
+&lt;button (click)="handleClick()">Обновить данные&lt;/button></code></pre>
+            <p>
+                В примере выше в конструкторе класса компонента отключается механизм <code>Change Detection</code>. Но
+                если нажать на кнопку, то в обработчике клика вызовется метод <code>detectChanges</code> и данные
+                обновятся.
+            </p>
+            <p>
+                Тут подвох в следующем. Когда запускается механизм <code>Change Detection</code>, то он идет от корня к
+                дочерним компонентам. После вызова метода <code>detach</code> мы как бы закрываем доступ к данному
+                компоненту и его дочерним компонентам извне. А метод <code>detectChanges</code> не идет от корня к
+                дочерним компонентам. Там, где он вызывается, образуется как бы локальный корень для механизма
+                <code>Change Detection</code> и от него уже идет проверка текущего компонента и его дочерних
+                компонентов.
+            </p>
+            <p>
+                Т.к. в примере рассмотрен корневой компонент, то локальный корневой компонент совпал с фактическим.
+                Т.е., механизм <code>Change Detection</code> запустился локально только для корневого компонента.
+            </p>`,
+    selected: false,
+    lastUpdate: '10.08.2024',
+};
