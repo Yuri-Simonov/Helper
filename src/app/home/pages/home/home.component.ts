16ebe51139d1d4a4ffc8ef3a5971022da282703b
Yuri Simonov <yura15-96@mail.ru>
2024-08-14 17:53:18 +0300

    рефакторинг навигации

--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { IChapter } from '../../types/chapter.interface';
+
+import { NAVIGATION } from '../../../shared/data/navigation';
+
+import { INavigation } from '../../../shared/types';
 
 @Component({
     selector: 'app-home',
@@ -8,41 +11,5 @@ import { IChapter } from '../../types/chapter.interface';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
-    chapters: IChapter[] = [
-        {
-            id: 'theory',
-            title: 'Теоретические материалы',
-            links: [
-                { path: 'theory/javascript', name: 'JavaScript' },
-                { path: 'theory/angular', name: 'Angular' },
-                { path: 'theory/git', name: 'Git' },
-                { path: 'theory/others', name: 'Разное' },
-            ],
-        },
-        {
-            id: 'courses',
-            title: 'Курсы',
-            links: [
-                {
-                    href: 'https://www.youtube.com/playlist?list=PL2bJ6t_D6_KSSiM2Y8T32-5KgaNzzS4R6',
-                    name: 'Angular',
-                },
-            ],
-        },
-        // {
-        //     id: 'tasks',
-        //     title: 'Задачи',
-        //     links: [{ path: '#', name: 'JavaScript', disabled: true }],
-        // },
-        {
-            id: 'quizzes',
-            title: 'Тесты',
-            links: [
-                { path: 'quizzes/javascript', name: 'JavaScript' },
-                { path: 'quizzes/angular', name: 'Angular' },
-                // { path: '#', name: 'Git', disabled: true },
-                // { path: '#', name: 'Разное', disabled: true },
-            ],
-        },
-    ];
+    chapters: INavigation[] = NAVIGATION;
 }
