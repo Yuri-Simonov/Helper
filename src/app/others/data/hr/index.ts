6d21d70fb379b79d30ca60b6258c4a7d22f248a1
Yuri Simonov <yura15-96@mail.ru>
2024-09-06 21:05:42 +0300

    добавлены вопросы от hr

--- src/app/others/data/hr/index.ts
+++ src/app/others/data/hr/index.ts
@@ -1,5 +1,24 @@
 import { IInfo } from '@types';
 
 import { QUIT } from './all/quit';
+import { SELECT_COMPANY } from './all/select-company';
+import { ABOUT_YOURSELF } from './all/about-yourself';
+import { STRENGTHS } from './all/strengths';
+import { DEADLINES } from './all/deadlines';
+import { TEAM } from './all/team';
+import { SELECT_FRONTEND } from './all/select-frontend';
+import { UNFAMILIAR_TECHNOLOGY } from './all/unfamiliar-technology';
 
-export const allOthersData: IInfo[] = [{ chapter: 'Прочее' }, QUIT];
+export const allOthersData: IInfo[] = [
+    { chapter: 'Вопросы от HR' },
+    QUIT,
+    SELECT_COMPANY,
+    ABOUT_YOURSELF,
+    STRENGTHS,
+    DEADLINES,
+    TEAM,
+    SELECT_FRONTEND,
+    UNFAMILIAR_TECHNOLOGY,
+    { chapter: 'Вопросы для HR' },
+    { chapter: 'Вопросы для технического специалиста' },
+];
