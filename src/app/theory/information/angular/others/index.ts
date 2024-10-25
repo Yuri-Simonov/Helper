17f09f8cdb4abd27255e88feb3f841ce8f1dddfe
Yuri Simonov <yura15-96@mail.ru>
2024-10-25 18:18:16 +0300

    правки по инфе DI

--- src/app/theory/information/angular/others/index.ts
+++ src/app/theory/information/angular/others/index.ts
@@ -3,7 +3,6 @@ import { IInfo } from '@types';
 import { ANGULAR_CLI } from './information/angular-cli';
 import { MODULES_IN_ANGULAR } from './information/modules-in-angular';
 import { COMPILATION_MECHANISMS } from './information/compilation-mechanisms';
-import { DEPENDENCY_INJECTION } from './information/dependency-injection';
 import { CONNECTING_SCRIPTS_AND_STYLES } from './information/connecting-scripts-and-styles';
 import { RENDERER_2 } from './information/renderer-2';
 import { SOME_GLOBAL_STYLE_FILES } from './information/some-global-style-files';
@@ -20,7 +19,6 @@ export const othersInfo: IInfo[] = [
     STYLE_SELECTORS,
     { chapter: 'Прочее' },
     COMPILATION_MECHANISMS,
-    DEPENDENCY_INJECTION,
     CONNECTING_SCRIPTS_AND_STYLES,
     RENDERER_2,
 ];
--- src/app/theory/information/angular/services/information/service-implementation.ts
+++ src/app/theory/information/angular/services/information/service-implementation.ts
@@ -6,16 +6,47 @@ export const SERVICE_IMPLEMENTATION: IInfo = {
                 <span class="attention"
                     >Чтобы получить данные из сервиса в каком-либо компоненте, его необходимо внедрить как
                     зависимость</span
-                >. Делается это с помощью паттерна <code>dependency injection</code>, который и переводится как
-                "внедрение зависимости".
+                >. Делается это с помощью паттерна <code>dependency injection</code> (или сокращенно <code>DI</code>),
+                который и переводится как "внедрение зависимости".
             </p>
-            <p>Внедрить сервис как зависимость можно разными способами:</p>
-            <i class="subtitle">Использование конструктора (расширенныq вариант)</i>
+            <p>
+                Суть паттерна <code>Dependency Injection</code> (<code>DI</code>) заключается в избежании прямых
+                зависимостей между классами. Осуществляется это за счет автоматической поставки необходимых зависимостей
+                в конструкторы классов, когда им нужны свойства или методы других классов. И раз мы разбираем Angular,
+                стоит уточнить, что речь идет про конструкторы классов различных сущностей: компонентов, директив,
+                сервисов и т.д.
+            </p>
+            <p>
+                В Angular за реализацию паттерна <code>DI</code> отвечает встроенный класс, который называется
+                <code>Injector</code>.
+            </p>
+            <p>
+                Самый распространенный случай использования паттерна <code>DI</code>, это когда в компоненты внедряются
+                сервисы и используется их функционал.
+            </p>
+            <p>
+                Допустим, у нас есть произвольный сервис <code>DataService</code>, который может предоставить нам
+                какие-то данные:
+            </p>
+            <pre><code class="language-typescript">@Injectable({
+    providedIn: 'root',
+})
+export class DataService {
+    private _data: string[] = ['One', 'Two', 'Three'];
+
+    getData() {
+        return this._data;
+    }
+}</code></pre>
+            <p>Внедрить данный сервис как зависимость можно разными способами:</p>
+            <i class="subtitle">Использование конструктора (расширенный вариант)</i>
             <pre><code class="language-typescript">export class AppComponent {
     private _dataService: DataService;
 
     constructor(dataService: DataService) {
         this._dataService = dataService;
+		// вывод данных из сервиса в консоль
+        console.log(this._dataService.getData()); // ['One', 'Two', 'Three']
     }
 }</code></pre>
             <p>Если делать по шагам, то:</p>
@@ -40,7 +71,10 @@ export const SERVICE_IMPLEMENTATION: IInfo = {
             <i class="subtitle">Использование конструктора (короткий вариант)</i>
             <p>Короткая запись будет выглядеть следующим образом:</p>
             <pre><code class="language-typescript">export class AppComponent {
-    constructor(private dataService: DataService) {}
+    constructor(private dataService: DataService) {
+		// вывод данных из сервиса в консоль
+        console.log(this.dataService.getData()); // ['One', 'Two', 'Three']
+	}
 }</code></pre>
             <p>
                 В конструкторе сразу указывается модификатор доступа и название свойства (<code>dataService</code>).
@@ -48,10 +82,15 @@ export const SERVICE_IMPLEMENTATION: IInfo = {
                 него как тип данных (<code>DataService</code>).
             </p>`,
     selected: false,
-    lastUpdate: '04.10.2024',
+    lastUpdate: '25.10.2024',
     footerLinks: [
         {
+            title: 'Внедрение зависимостей по шагам',
             path: 'https://youtu.be/oOrI3qm7tK8?t=123',
+        },
+        {
+            title: 'Теория DI',
+            path: 'https://youtu.be/mFem0yjCkn4?t=16',
         },
     ],
 };
