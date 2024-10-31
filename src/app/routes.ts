3a9cce17e4b7d9b3fb74af3b78903a81d180dcfd
Yuri Simonov <yura15-96@mail.ru>
2024-10-31 16:24:39 +0300

    добавлена страница с настройками. Выбор темы перенес из хедера в настройки

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 
 import { ForExamplesComponent } from './for-examples/for-examples/for-examples.component';
+import { SettingsComponent } from './shared/components/settings/settings.component';
 
 export const appModuleRoutes: Routes = [
     {
         path: 'for-examples',
         component: ForExamplesComponent,
+    },
+    {
+        path: 'settings',
+        component: SettingsComponent,
     },
     { path: '**', redirectTo: '' },
 ];
