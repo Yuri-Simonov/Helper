1e6c8307c72925c205ea538315d02704886551ae
Yuri Simonov <yura15-96@mail.ru>
2025-01-06 11:56:09 +0300

    добавлена задача по js 'Формирование диапазонов из массива чисел'

--- src/app/tasks/tasks/javascript/index.ts
+++ src/app/tasks/tasks/javascript/index.ts
@@ -4,6 +4,7 @@ import { CALCULATOR_OBJECT } from './all/calculator-object';
 import { FLATTEN_NESTED_ARRAY } from './all/flatten-nested-array';
 import { IMPLEMENT_DELAY_FUNCTION } from './all/implement-delay-function';
 import { PALINDROME_CHECK } from './all/palindrome-check';
+import { GENERATE_RANGES } from './all/generate-ranges';
 
 export const allJavaScriptTasks: IInfo[] = [
     { chapter: 'Задачи с собеседований' },
@@ -11,4 +12,5 @@ export const allJavaScriptTasks: IInfo[] = [
     FLATTEN_NESTED_ARRAY,
     IMPLEMENT_DELAY_FUNCTION,
     PALINDROME_CHECK,
+    GENERATE_RANGES,
 ];
