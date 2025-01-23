bd5f3f41649e01c889ffa0c99edd3d61256bfd4a
Yuri Simonov <yura15-96@mail.ru>
2025-01-23 17:32:52 +0300

    Auto-generated commit

---
+++ chunk-BUCMLGPO.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-RGP5VD5C.js";import"./chunk-4CLCTAJ7.js";export{a as servicesInfo};
--- chunk-CXYQ4REQ.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-Q2AKTDBP.js";import"./chunk-4CLCTAJ7.js";export{a as servicesInfo};
